fbd1be011b2f5e6b1db7a32bc9a2807794319401
brunobc <brunobezerrachaves@gmail.com>
2016-02-01 00:57:18 -0300

    Aula 6

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,11 +1,25 @@
 angular.module('alurapic').controller('FotosController', function ($scope, $http, $log) {
-	$scope.fotos = [];
+  $scope.fotos = [];
 
-	$http.get('v1/fotos')
-	.success(function (data) {
-		$scope.fotos = data;
-		$log.info(data);
-	}).error(function(erro) {
-		$log.error(erro);
-	});
+  $http.get('v1/fotos')
+  .success(function (data) {
+    $scope.fotos = data;
+    $log.info(data);
+  }).error(function(erro) {
+    $log.error(erro);
+  });
+
+  $scope.submeter = function () {
+    $http.post('/V1/fotos', $scope.foto)
+    .success(function() {
+      console.log('Foto add');
+      $scope.mensagem = 'Foto cadastrada com sucesso';
+    })
+    .error(function(erro) {
+      console.log('Não foi possível cadastrar');
+      $scope.mensagem = 'Não foi possível cadastrar a foto';
+    })
+
+  };
+
 });
